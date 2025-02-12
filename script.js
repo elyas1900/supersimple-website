d40699857e1d804a19e0edb5c62b2e3155884588
elyas1900 <elyastaheri31@gmail.com>
2025-02-12 12:12:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -45,16 +45,3 @@ closeBtn.addEventListener('click', () => {
 
 document.addEventListener("DOMContentLoaded", function () {
     const elements = document.querySelectorAll(".hidden");
-
-    function checkScroll() {
-        elements.forEach(el => {
-            if (el.getBoundingClientRect().top < window.innerHeight - 100) {
-                el.classList.add("show");
-            }
-        });
-    }
-
-    window.addEventListener("scroll", checkScroll);
-});
-
-
